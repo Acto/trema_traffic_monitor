71164ff1e6777a5cbbe417613bb57951435543ce
kota.sugihara <kou.cshu@gmail.com>
2015-07-08 20:13:14 +0900

    match

--- analyze_wg_log.rb
+++ analyze_wg_log.rb
@@ -1,13 +1,15 @@
 class WgLog
-    attr_accessor :log, :log_arr, :seq
+    attr_accessor :log, :log_arr, :seq, :msg
 
   def initialize(text)
     @log = text
     @log_arr = @log.split(" ")
     @seq = @log_arr[0].match(%r{seq=(\d+)})[1].to_i
+    @msg = @log_arr[4].match(%r{msg="(\w+)"})
   end
 end
 
 # Creatureクラスのnewメソッドでオブジェクトを生成する
-wl = WgLog.new('seq=20000 2015-01-02 01:02:11 Deny')
+wl = WgLog.new('seq=20000 2015-01-02 01:02:11 Deny msg="DDoS"')
 puts wl.seq
+puts wl.msg
