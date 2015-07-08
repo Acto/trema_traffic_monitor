7509e06174232593cde5b6ccd95dc9e1f8b6e945
kota.sugihara <kou.cshu@gmail.com>
2015-07-08 21:20:49 +0900

    pipe + json output

--- analyze_wg_log.rb
+++ analyze_wg_log.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class WgLog
     attr_accessor :log, :log_arr, :seq, :msg
 
@@ -13,3 +15,36 @@ end
 wl = WgLog.new('seq=20000 2015-01-02 01:02:11 Deny msg="DDoS"')
 puts wl.seq
 puts wl.msg
+
+log_id = 1
+
+current_data = {
+    id: 1,
+    data1: 500,
+    data2: 0
+}
+
+bef_data = current_data.clone
+offset_data = current_data.clone
+
+File.foreach("wg_pipe") {|x|
+
+    print "GOT ", x
+    arr = x.split(",")
+
+    log_id += 1
+
+    current_data[:id] = log_id
+    current_data[:data1] = arr[1].to_i
+    current_data[:data2] = arr[2].to_i
+
+    current_data.each{|key, current_value|
+        if key != :id
+            offset_data[key] = current_value > bef_data[key] ? current_value - bef_data[key] : current_value
+        else
+            offset_data[key] = current_value + 1
+        end
+    }
+
+    File.open("data.json", "wb") {|f| f.write JSON.generate(offset_data) }
+}
